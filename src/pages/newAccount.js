bcb5c6230b88a8fd96f103cb04ed6cce748f99c0
Rashaad Gray <grayrashaad@outlook.com>
2022-08-25 10:57:21 -0500

    can create new account

--- src/pages/newAccount.js
+++ src/pages/newAccount.js
@@ -5,17 +5,33 @@ import React, { useState, useEffect } from "react";
 
 export const NewAccount = (props) => {
   const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [pin, setPin] = useState("");
+  const [cash, setCash] = useState(0);
+  const [available, setAvailable] = useState("");
   const [user, setUser] = useState([]);
+  const [element, setElement] = useState("");
   
+  const userNameCheck = (e) => {
+ 
+    const API_URL = "http://localhost:8080/user/available/" + username;
+    axios.get(API_URL).then((res) => {
+      const result = res.data;
+      setAvailable(result);
+      console.log(available);
+    });
+
+  }
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(username);
     console.log(password);
+    console.log(user);
 
-    const API_URL = "http://localhost:8080/user/login";
-    axios.post(API_URL, {"username": username, "password": password}, {headers: {"Content-Type": "application/json"}} ).then((res) => {
+    const API_URL = "http://localhost:8080/user/new";
+    axios.post(API_URL, {"first_name": firstName, "last_name": lastName, "username": username, "pass": password, "pin": pin, "cash": cash}, {headers: {"Content-Type": "application/json"}} ).then((res) => {
       const result = res.data;
       setUser(result);
     });
@@ -24,6 +40,23 @@ export const NewAccount = (props) => {
   {
     let url = "/userHome";
     window.location.href = url;
+  }
+
+  if(user == false)
+  {
+    setElement(
+      <div>
+        <h2>Error</h2>
+      </div>
+    );
+  }
+  else
+  {
+    setElement(
+      <div>
+        <h2></h2>
+      </div>
+    );
   }
  
   };
@@ -37,22 +70,37 @@ export const NewAccount = (props) => {
 
       <form onSubmit={handleSubmit} action="/">
         <label>First Name:</label>
-        <input type="text" id="firstName" name="firstName" onChange={(event) => setUsername(event.target.value)} value={firstName}></input>
+        <input type="text" id="firstName" name="firstName" required onChange={(event) => setFirstName(event.target.value)} value={firstName}></input>
+
+        <br></br>
+
+        <label>Last Name:</label>
+        <input type="text" id="lastName" name="lastName" required onChange={(event) => setLastName(event.target.value)} value={lastName}></input>
 
         <br></br>
 
         <label>Username:</label>
-        <input type="text" id="username" name="username" onChange={(event) => setUsername(event.target.value)} value={username}></input>
+        <input type="text" id="username" name="username" required onChange={(event) => {setUsername(event.target.value); userNameCheck(event.target.value);} } value={username}></input>
 
         <br></br>
 
         <label>Password:</label>
-        <input type="text" id="password" name="password" onChange={(e) => setPassword(e.target.value)} value={password}></input>
+        <input type="text" id="password" name="password" required onChange={(e) => setPassword(e.target.value)} value={password}></input>
+
+        <br></br>
+
+        <label>Pin:</label>
+        <input type="text" id="pin" name="pin" pattern="^\d\d\d\d$" required onChange={(e) => setPin(e.target.value)} value={pin}></input>
+
+        <br></br>
+
+        <label>Initial Deposit:</label>
+        <input type="number" id="cash" name="cash" min="1" max="9999999999" step=".01" required onChange={(e) => setCash(e.target.value)} value={cash}></input>
         
         <br></br>
         <input type="submit" value="newAccount" />
       </form>
-
+      {element}
     </div>
     
   );
